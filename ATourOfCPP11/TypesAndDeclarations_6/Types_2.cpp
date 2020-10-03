79aa737f8cf1e82f67b4eb512c1c22cd96d22d98
Masoudas <Masoudas@hotmail.com>
2020-10-03 13:27:29 +0200

    More on casts

--- ATourOfCPP11/TypesAndDeclarations_6/Types_2.cpp
+++ ATourOfCPP11/TypesAndDeclarations_6/Types_2.cpp
@@ -1,12 +1,12 @@
 /**
  * C++ holds the most basic types. These are bool, char, int, long, float, double, and void.
- * We sometimes initialize built-in types in brackets. This sometimes help unexpected operations, like
+ * We sometimes initialize built-in types in brackets. This sometimes help avoid unexpected operations, like
  * truncating floating point types.
  * int x {10};  // x is an int with value  10.
  * bool b1 {true};
  * 
  * a bool can be assigned to int. In that case, false would be zero, and true would be one.
- * On the other hand, a zero is assigned to false, and any positive int is assigned to true.
+ * On the other hand, a zero is assigned to false, and any non-zero int is assigned to true.
  * int i2 {true};//i2 becomes 1
  * bool b {10}; // b would be true.
  * 
